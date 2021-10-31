32a1b5cb678c8868deea1a8f0e160cdd26a14566
wzhaofei <505139165@qq.com>
2021-10-31 21:39:07 +0800

    feat: (20211031) add insertionSort

---
+++ phase01/20211031/insertionSort.js
@@ -0,0 +1,17 @@
+function insertionSort(arr) {
+    let len = arr.length;
+    let preIndex, current;
+    for (let i = 1; i < len; i++) {
+        preIndex = i - 1;
+        current = arr[i];
+        while (preIndex >= 0 && arr[preIndex] > current) {
+            arr[preIndex + 1] = arr[preIndex];
+            preIndex--;
+        }
+        arr[preIndex + 1] = current;
+    }
+    return arr;
+}
+const arr = [3, 5, 2, 1, 4, 6, 7, 8, 9]
+const result = insertionSort(arr);
+console.log(result)
