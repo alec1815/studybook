a12b86d646fb2e9e95e0ad5e24a3b541bd274f2c
wzhaofei <505139165@qq.com>
2021-11-03 00:10:44 +0800

    feat: (all) leetcode 141. 环形链表 解题1

---
+++ phase01/20211102/hasCycle/hasCycle1.js
@@ -0,0 +1,17 @@
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let pointer = head;
+    let map = new Map();
+    while (pointer !== null) {
+      if (map.has(pointer)) {
+        return true; 
+      } else {
+        map.set(pointer);
+        pointer = pointer.next; 
+      }
+    }
+    return false; 
+};
