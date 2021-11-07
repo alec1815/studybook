ef1d629f00ef9ea3ac00e1264e0720770eeca8bc
wzhaofei <505139165@qq.com>
2021-11-07 23:14:40 +0800

    feat: (all) detectCycle

---
+++ phase01/20211103/detectCycle/detectCycle.js
@@ -0,0 +1,17 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var detectCycle = function(head) {
+    let pointer = head;
+    let map = new Map();
+    while (pointer !== null) {
+      if (map.has(pointer)) {
+        return pointer; 
+      } else {
+        map.set(pointer);
+        pointer = pointer.next; 
+      }
+    }
+    return null; 
+  };
---
+++ phase01/20211103/detectCycle/detectCycle2.js
@@ -0,0 +1,22 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var detectCycle = function(head) {
+      if(head == null || head.next == null) return null;
+      let fast = head;
+      let slow = head;
+      while(fast !== null && fast.next !== null){
+          fast = fast.next.next;
+          slow = slow.next;
+          if(fast == slow){
+              let temp = head;
+              while(temp !== fast){
+                  fast = fast.next;
+                  temp = temp.next;
+              }
+              return  fast;
+          }
+      }
+      return null;
+  };
